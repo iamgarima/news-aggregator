6d1c4b063b5c437294679f8454c1afbcef07e8bb
Garima Kamboj <garima.kamboj@icloud.com>
2020-08-18 08:52:53 +0530

    Setup story-card

---
+++ src/components/story-card/story-card.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './story-card.module.css';
+
+const StoryCard = props => {
+  return <div className={styles.wrapper}>
+    Hello
+  </div>;
+};
+
+export default StoryCard; 
