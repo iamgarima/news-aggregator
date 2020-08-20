80428feeef0f5e77e624548f0bb3adf9d5226993
Garima Kamboj <garima.kamboj@icloud.com>
2020-08-20 21:30:38 +0530

    Add styles to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import Select from 'react-select';
 import languageOptions from './language-options';
 import { updateLanguage } from '../../actions/language';
+import styles from './header.module.css';
 
 const Header = () => {
   const [inputVal, updateInputVal] = useState("");
@@ -16,18 +17,22 @@ const Header = () => {
     dispatch(updateLanguage({ code: value, name: label }));
   };
 
-  return <div>
-    <div>
-      <input type="text" onChange={e => updateInputVal(e.target.value)} />
-      <Link to={`/search?query=${inputVal}`}>Search</Link>
-      <Select
-        value={langOption}
-        onChange={selectHandler}
-        options={languageOptions}
-        isSearchable
-      />
-    </div>
-  </div>
+  return <div className={styles.headerWrapper}>
+      <div className={styles.searchWrapper}>
+        <input className={styles.inputBox} type="text" onChange={e => updateInputVal(e.target.value)} />
+        <Link to={`/search?query=${inputVal}`}>
+          <button className={styles.searchBtn}>Search</button>
+        </Link>
+      </div>
+      <div className={styles.selectWrapper}>
+        <Select
+          value={langOption}
+          onChange={selectHandler}
+          options={languageOptions}
+          isSearchable
+        />
+      </div>
+    </div>;
 };
 
 export default Header;
