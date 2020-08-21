c6f1272d170eb4e83e4d2e9ec70ea06e98931249
Garima Kamboj <garima.kamboj@icloud.com>
2020-08-21 09:05:13 +0530

    Make header responsive

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -23,13 +23,15 @@ const Header = () => {
   const errorMessage = (geolocation.status === "error" && geolocation.error.code === 0) ? `Sorry! Can't see weather details? ${geolocation.error.message}` : "To see weather details, please make sure your geolocation is enabled and then refresh the page.";
 
   return <div className={styles.headerWrapper}>
-      {pageType === "search-page" && <Link to="/" className={styles.homeIcon}>
-          <svg fill="#000000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32px" height="32px"><path d="M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 11 21 L 11 15 L 13 15 L 13 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z M 12 4.7910156 L 18 10.191406 L 18 11 L 18 19 L 15 19 L 15 13 L 9 13 L 9 19 L 6 19 L 6 10.191406 L 12 4.7910156 z"/></svg>
-      </Link>}
+      {pageType === "search-page" && <div className={styles.homeIconWrapper}>
+        <Link to="/" className={styles.homeIcon}>
+            <svg fill="#000000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32px" height="32px"><path d="M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 11 21 L 11 15 L 13 15 L 13 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z M 12 4.7910156 L 18 10.191406 L 18 11 L 18 19 L 15 19 L 15 13 L 9 13 L 9 19 L 6 19 L 6 10.191406 L 12 4.7910156 z"/></svg>
+        </Link>
+      </div>}
       <div className={styles.searchWrapper}>
         <input className={styles.inputBox} type="text" onChange={e => updateInputVal(e.target.value)} />
-        <Link to={`/search?query=${inputVal}`}>
-          <button className={styles.searchBtn}>Search</button>
+        <Link to={`/search?query=${inputVal}`} className={styles.searchBtn}>
+          Search
         </Link>
       </div>
       <div className={styles.selectWrapper}>
